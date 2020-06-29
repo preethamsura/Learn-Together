3640ec8cc39f727ff6a2dab569ecd2d933790df5
uthmanmomen13 <livininthemomen13@gmail.com>
2020-06-29 09:27:13 -0700

    more server.js changes

--- LearnTogether/backend/server.js
+++ LearnTogether/backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Initialize http server
 const app = express();
-
+const port = process.env.Port || 80;
+app.use(cors())
+app.use(express.json());
 // Handle / route
 app.get('/', (req, res) =>
   res.send('<h1>Hello Bryan!</h1>')
 )
 
 // Launch the server on port 3000
-const server = app.listen(80, () => {
-  const { address, port } = server.address();
+const server = app.listen(port, () => {
+  const { address, port_ } = server.address();
   console.log(`Listening at http://${address}:${port}`);
 });
