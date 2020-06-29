61a55f7d8b830e2f4e78c0880811fb4e7a25a946
uthmanmomen13 <livininthemomen13@gmail.com>
2020-06-29 09:18:01 -0700

    fixed package-lock and got server.js setup

--- LearnTogether/backend/server.js
+++ LearnTogether/backend/server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+// Initialize http server
+const app = express();
+
+// Handle / route
+app.get('/', (req, res) =>
+  res.send('<h1>Hello Bryan!</h1>')
+)
+
+// Launch the server on port 3000
+const server = app.listen(80, () => {
+  const { address, port } = server.address();
+  console.log(`Listening at http://${address}:${port}`);
+});
